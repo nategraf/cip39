0e4fe31d4f20898d597da1c079e6ffac1ac87b4b
Victor Graf <victor@clabs.co>
2021-06-04 14:55:11 -0700

    make usage of k consistent with proposal

--- cip39.py
+++ cip39.py
@@ -12,8 +12,11 @@ def validate_n_k(n, k):
     if n % 3 != 0 or k % 3 != 0:
         return False, "n and k must be divisible by 3"
 
-    if n < 12 or n > 24 or k < 0 or (n-k) < 12:
-        return False,  "n at most 24 and at least 12. k must be at least 0 and n-k must be at least 12."
+    if n < 12 or n > 24:
+        return False, "n must be at least 12 and at most 24"
+
+    if k < 12 or k > n:
+        return False, "k must be at least 12 and at most n"
 
     return True, None
 
@@ -32,7 +35,7 @@ def symbols_to_mnemonic(symbols, mnemonic):
     return ' '.join(mnemonic.wordlist[s] for s in symbols)
 
 def mnemonic_to_symbols(phrase, mnemonic):
-    words = phrase.split(' ')
+    words = phrase.split()
     symbols = [-1] * len(words)
     for i, word in enumerate(words):
         if word not in mnemonic.wordlist:
@@ -42,21 +45,24 @@ def mnemonic_to_symbols(phrase, mnemonic):
 
     return symbols
 
-def generate(n, k=3, mnemonic=Mnemonic("english")):
-    """generate a mnemonic phrase with the specified level of error correction
+def generate(n, k=12, mnemonic=Mnemonic("english")):
+    """Generate a mnemonic phrase with the specified level of error correction.
+
+    Phrases include t=(n-k) "error correcting" words, allowing the correction of up to t invalid
+    words in a phrase, and up to floor(t/2) words that are wrong, but still valid BIP-39 words.
 
     Arguments:
         n (int): Total number of words in the phrase.
-        k (int): Number of words devoted to error correction.
-        mnemonic (Mnemonic): Istance of Mnemonic to use.
+        k (int): Number of words chosen at random.
+        mnemonic (Mnemonic): Instance of Mnemonic to use.
     """
     ok, error = validate_n_k(n, k)
     if not ok:
         raise ValueError(error)
 
-    coder = RSCodec(nsize=n, nsym=k, c_exp=BIP39_SYMBOL_SIZE)
+    coder = RSCodec(nsize=n, nsym=(n-k), c_exp=BIP39_SYMBOL_SIZE)
     for i in itertools.count():
-        bits = random_bits((n-k)*BIP39_SYMBOL_SIZE)
+        bits = random_bits(k*BIP39_SYMBOL_SIZE)
         symbols = bits_to_symbols(bits, BIP39_SYMBOL_SIZE)
         coded = coder.encode(symbols)
         phrase = symbols_to_mnemonic(coded, mnemonic)
@@ -65,17 +71,29 @@ def generate(n, k=3, mnemonic=Mnemonic("english")):
 
         return phrase
 
-def recover(phrase, n, k, mnemonic=Mnemonic("english")):
-    ok, error = validate_n_k(n, k)
+def recover(phrase, k=12, mnemonic=Mnemonic("english")):
+    """Attempts to recover the original mnemonic phrase from a mnemonic phrase with errors.
+
+    Infers the value of n from the number of words in the phrase. If words are missing or unknown,
+    include an arbitrary character in its place. (e.g. '_')
+
+    Arguments:
+        phrase (str): Mnemonic phrase with errors to fix.
+        k (int): Number of words chosen at random.
+        mnemonic (Mnemonic): Instance of Mnemonic to use.
+    """
+    symbols = mnemonic_to_symbols(phrase, mnemonic)
+    ok, error = validate_n_k(len(symbols), k)
     if not ok:
         raise ValueError(error)
 
-    symbols = mnemonic_to_symbols(phrase, mnemonic)
-    if len(symbols) != n:
-        raise ValueError("number of words in mnemonic must match n")
-
-    coder = RSCodec(nsize=n, nsym=k, c_exp=BIP39_SYMBOL_SIZE)
+    coder = RSCodec(nsize=len(symbols), nsym=(len(symbols)-k), c_exp=BIP39_SYMBOL_SIZE)
     erasures = [i for i, s in enumerate(symbols) if s < 0]
     recovered, _, _ = coder.decode(symbols, erase_pos=erasures)
     coded = coder.encode(recovered)
-    return symbols_to_mnemonic(coded, mnemonic)
+    phrase = symbols_to_mnemonic(coded, mnemonic)
+
+    if not mnemonic.check(phrase):
+        raise ValueError("error-corrected phrase does not have a valid checksum")
+
+    return phrase
